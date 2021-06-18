c9a8368b64d22422a0bdd8c87ccbbf91c291953b
ADILZAMAL <adilzamal@gmail.com>
2021-06-19 01:37:50 +0530

    details and carousel component

--- src/useBreedList.js
+++ src/useBreedList.js
@@ -24,7 +24,7 @@ const useBreedList = (animal) => {
       setBreedList(localCache[animal]);
       setStatus("loaded");
     }
-  }, [animal]);
+  }, [animal]); //eslint-disable-line
 
   return [breedList, status];
 };
