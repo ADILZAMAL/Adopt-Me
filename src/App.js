28abf682565b08d416047e5324756a35f65724a1
ADILZAMAL <adilzamal@gmail.com>
2021-06-18 10:25:25 +0530

    Search params component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
-import Pet from "./Pet";
 import ReactDOM from "react-dom";
+import SearchParams from "./SearchParams";
 
 const App = () => {
   return (
     <div>
       <h1>Adopt Me!</h1>
-      <Pet name="Luna" animal="Dog" breed="Havanese" />
-      <Pet name="Ruma" animal="Bird" breed="Ramsa" />
-      <Pet name="Sandy" animal="Cow" breed="Mix" />
+      <SearchParams />
     </div>
   );
 };
