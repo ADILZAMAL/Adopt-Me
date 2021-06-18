bef92c0d0b9cb67d0acec38db2a92c016c73c8f0
ADILZAMAL <adilzamal@gmail.com>
2021-06-18 10:02:08 +0530

    setup eslint for react

--- src/App.js
+++ src/App.js
@@ -1,26 +1,15 @@
-/* global React ReactDOM */
-const Pet = (props) => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, props.name),
-    React.createElement("h2", {}, props.animal),
-    React.createElement("h2", {}, props.breed),
-  ]);
-};
+import Pet from "./Pet";
+import ReactDOM from "react-dom";
 
 const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(Pet, {
-      name: "Luna",
-      animal: "Dog",
-      breed: "Havanese",
-    }),
-    React.createElement(Pet, {
-      name: "Twinkle",
-      animal: "Bird",
-      breed: "Ramsa",
-    }),
-    React.createElement(Pet, { name: "Sandy", animal: "Cow", breed: "Mix" }),
-  ]);
+  return (
+    <div>
+      <h1>Adopt Me!</h1>
+      <Pet name="Luna" animal="Dog" breed="Havanese" />
+      <Pet name="Ruma" animal="Bird" breed="Ramsa" />
+      <Pet name="Sandy" animal="Cow" breed="Mix" />
+    </div>
+  );
 };
-ReactDOM.render(React.createElement(App), document.getElementById("root"));
+
+ReactDOM.render(<App />, document.getElementById("root"));
