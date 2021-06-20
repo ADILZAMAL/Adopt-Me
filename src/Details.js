50bf93da7247653c5fd910d3e7d4ec0f59f2f576
ADILZAMAL <adilzamal@gmail.com>
2021-06-20 08:32:43 +0530

    modal for adoption confirming

--- src/Details.js
+++ src/Details.js
@@ -1,13 +1,16 @@
 import { Component } from "react";
-import { withRouter } from "react-router-dom";
+import { withRouter, Redirect } from "react-router-dom";
 import Carousel from "./Carousel";
 import ErrorBoundaries from "./ErrorBoundaries";
 import ThemeContext from "./ThemeContext";
+import Modal from "./Modal";
 
 class Details extends Component {
   constructor() {
     super();
-    this.state = { loading: true };
+    this.state = { loading: true, showModal: false, redirect: false };
+    this.toggleModal = this.toggleModal.bind(this);
+    this.adopt = this.adopt.bind(this);
   }
   async componentDidMount() {
     console.log(this.props);
@@ -17,11 +20,24 @@ class Details extends Component {
     const json = await res.json();
     this.setState(Object.assign({ loading: false }, json.pets[0]));
   }
+
+  toggleModal = () => {
+    this.setState({ showModal: !this.state.showModal });
+  };
+
+  adopt = () => {
+    this.toggleModal();
+    window.alert("Pet Adopted");
+    this.setState({ redirect: true });
+  };
   render() {
     if (this.state.loading) {
       return <h2>Loading...</h2>;
+    } else if (this.state.redirect) {
+      return <Redirect to="/details" />;
     }
-    const { animal, breed, city, state, description, name, images } =
+
+    const { animal, breed, city, state, description, name, images, showModal } =
       this.state;
 
     return (
@@ -32,11 +48,27 @@ class Details extends Component {
           <h2>{`${animal} — ${breed} — ${city}, ${state}`}</h2>
           <ThemeContext.Consumer>
             {([theme]) => (
-              <button style={{ backgroundColor: theme }}>Adopt {name}</button>
+              <button
+                onClick={this.toggleModal}
+                style={{ backgroundColor: theme }}
+              >
+                Adopt {name}
+              </button>
             )}
           </ThemeContext.Consumer>
           <p>{description}</p>
         </div>
+        {showModal ? (
+          <Modal>
+            <div>
+              <h1>Would you like to adopt {name}?</h1>
+              <div className="buttons">
+                <button onClick={this.adopt}>Yes</button>
+                <button onClick={this.toggleModal}>No</button>
+              </div>
+            </div>
+          </Modal>
+        ) : null}
       </div>
     );
   }
---
+++ src/Modal.js
@@ -0,0 +1,7 @@
+import ReactDOM from "react-dom";
+export default function Modal({ children }) {
+  return ReactDOM.createPortal(
+    <div>{children}</div>,
+    document.getElementById("modal")
+  );
+}
